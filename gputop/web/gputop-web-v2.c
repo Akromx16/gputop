0c699458dbf395ad2751487d6d5c98203b9b8201
Sergio Martinez <sergio.martinez.rodriguez@intel.com>
2016-01-14 11:51:12 +0000

    Working on the emscripten part of the web

--- gputop/web/gputop-web-v2.c
+++ gputop/web/gputop-web-v2.c
@@ -31,23 +31,20 @@
 #include <sys/types.h>
 
 #include <uuid/uuid.h>
-
 #include <emscripten.h>
+#include <emscripten/bind.h>
 
 #include <intel_chipset.h>
 #include <i915_oa_drm.h>
 
-#include <gputop-perf.h>
 #include <gputop-web.h>
 #include <gputop-string.h>
-#include <gputop.pb-c.h>
 #include <gputop-oa-counters.h>
 
 #include "oa-hsw.h"
 #include "oa-bdw.h"
 #include "oa-chv.h"
 #include "oa-skl.h"
-
 
 struct gputop_perf_query i915_perf_oa_queries[I915_OA_METRICS_SET_MAX];
 
@@ -74,6 +71,25 @@ struct gputop_worker_query {
     gputop_list_t link;
 };
 
+/*
+EMSCRIPTEN_BINDINGS(my_value_example) {
+    value_object<gputop_worker_query>("gputop_worker_query")
+        .field("id", &gputop_worker_query::id)
+        .field("aggregation_period", &gputop_worker_query::aggregation_period)
+
+        .value_array<oa_clock>("oa_clock")
+             .element(&oa_clock::start)
+             .element(&oa_clock::timestamp)
+             .element(&oa_clock::last_raw)
+
+        .field("start_timestamp", &gputop_worker_query::start_timestamp)
+        .field("start_timestamp", &gputop_worker_query::end_timestamp)
+        ;
+
+    function("findPersonAtLocation", &findPersonAtLocation);
+}
+*/
+
 static gputop_list_t open_queries;
 
 struct oa_sample {
@@ -83,7 +99,6 @@ struct oa_sample {
 
 
 static struct gputop_devinfo devinfo;
-static int socket = 0;
 
 static int next_rpc_id = 1;
 
@@ -145,7 +160,13 @@ static void
 oa_clock_init(struct oa_clock *clock, uint32_t raw_start)
 {
     clock->timestamp = clock->start = (uint64_t)raw_start * 80;
-    //gputop_web_console_log("oa_clock_init: start=%"PRIu32" timestamp=%"PRIu64, raw_start, clock->timestamp);
+    //gputop_web_console_log("oa_clock_in       counter = &query->counters[query->n_counters++];
+    counter->oa_counter_read_float = skl__tdl_2__thread_header00_ready_port1__read;
+    counter->name = "Thread Header Ready on Slice0 Subslice0 Thread Dispatcher Port 1";
+    counter->desc = "The percentage of time in which thread header is ready on slice0 subslice0 thread dispatcher port 1";
+    counter->type = GPUTOP_PERFQUERY_COUNTER_RAW;
+    counter->data_type = GPUTOP_PERFQUERY_COUNTER_DATA_FLOAT;
+    counter->max = percentage_max_callback;it: start=%"PRIu32" timestamp=%"PRIu64, raw_start, clock->timestamp);
     clock->last_raw = raw_start;
 }
 
@@ -229,6 +250,7 @@ append_raw_oa_counter(gputop_string_t *str,
 static void
 forward_query_update(struct gputop_worker_query *query)
 {
+    /*
     struct gputop_perf_query *oa_query = query->oa_query;
     uint64_t delta;
     int i;
@@ -271,6 +293,7 @@ forward_query_update(struct gputop_worker_query *query)
 
     _gputop_web_worker_post(str->str);
     gputop_string_free(str, true);
+    */
 }
 
 static void
@@ -285,11 +308,11 @@ notify_bad_report(struct gputop_worker_query *query, uint64_t last_timestamp)
 
     gputop_string_append_printf(str, "} ], \"id\": %u }\n", next_rpc_id++);
 
-    _gputop_web_worker_post(str->str);
+    //_gputop_web_worker_post(str->str);
     gputop_string_free(str, true);
 }
 
-static void
+static void EMSCRIPTEN_KEEPALIVE
 handle_perf_message(int id, uint8_t *data, int len)
 {
     struct gputop_worker_query *query;
@@ -304,7 +327,7 @@ handle_perf_message(int id, uint8_t *data, int len)
     gputop_web_console_log("received perf data for unknown query id: %d", id);
 }
 
-static void
+static void EMSCRIPTEN_KEEPALIVE
 handle_oa_query_i915_perf_data(struct gputop_worker_query *query, uint8_t *data, int len)
 {
     struct gputop_perf_query *oa_query = query->oa_query;
@@ -430,7 +453,7 @@ handle_oa_query_i915_perf_data(struct gputop_worker_query *query, uint8_t *data,
     }
 }
 
-static void
+static void EMSCRIPTEN_KEEPALIVE
 handle_i915_perf_message(int id, uint8_t *data, int len)
 {
     struct gputop_worker_query *query;
@@ -488,259 +511,42 @@ data_counter_type_name(gputop_counter_data_type_t type)
     return "unknown";
 }
 
-static void
-append_i915_oa_query(gputop_string_t *str, struct gputop_perf_query *query)
-{
-    int i;
+static void EMSCRIPTEN_KEEPALIVE
+update_features(uint32_t devid, uint64_t n_eus, uint64_t n_eu_slices,
+        uint64_t n_eu_sub_slices, uint64_t eu_threads_count,
+        uint64_t subslice_mask, uint64_t slice_mask) {
 
-    gputop_string_append(str, "  {\n");
-    gputop_string_append_printf(str, "    \"name\": \"%s\",\n", query->name);
-    gputop_string_append_printf(str, "    \"metric_set\": %d,\n", query->perf_oa_metrics_set);
-    gputop_string_append_printf(str, "    \"counters\": [\n");
+    devinfo.devid = devid;
+    devinfo.n_eus = n_eus;
+    devinfo.n_eu_slices = n_eu_slices;
+    devinfo.n_eu_sub_slices = n_eu_sub_slices;
+    devinfo.eu_threads_count = eu_threads_count;
+    devinfo.subslice_mask = subslice_mask;
+    devinfo.slice_mask = slice_mask;
 
-    for (i = 0; i < query->n_counters; i++) {
-	struct gputop_perf_query_counter *counter = &query->counters[i];
-
-	switch (counter->type) {
-	case GPUTOP_PERFQUERY_COUNTER_EVENT:
-	case GPUTOP_PERFQUERY_COUNTER_DURATION_NORM:
-	case GPUTOP_PERFQUERY_COUNTER_DURATION_RAW:
-	case GPUTOP_PERFQUERY_COUNTER_THROUGHPUT:
-	case GPUTOP_PERFQUERY_COUNTER_RAW:
-	case GPUTOP_PERFQUERY_COUNTER_TIMESTAMP:
-	    gputop_string_append_printf(str, "      { \"name\": \"%s\", ", counter->name);
-	    gputop_string_append_printf(str, "\"index\": %u, ", i);
-	    gputop_string_append_printf(str, "\"description\": \"%s\", ", counter->desc);
-	    gputop_string_append_printf(str, "\"type\": \"%s\", ",
-					counter_type_name(counter->type));
-	    gputop_string_append_printf(str, "\"data_type\": \"%s\" ",
-					data_counter_type_name(counter->data_type));
-	    gputop_string_append(str, "}");
-	    break;
-	}
-
-	if (i < (query->n_counters - 1))
-	    gputop_string_append(str, ",\n");
-	else
-	    gputop_string_append(str, "\n");
-    }
-
-    gputop_string_append(str, "    ]\n");
-    gputop_string_append(str, "  }\n");
-}
-
-static void
-send_pb_message(int socket, ProtobufCMessage *pb_message)
-{
-    int len = protobuf_c_message_get_packed_size(pb_message);
-    void *buf = malloc(len);
-
-    protobuf_c_message_pack(pb_message, buf);
-
-    gputop_web_console_log("sent from web worker len=%d\n", len);
-    _gputop_web_socket_post(socket, buf, len);
-
-    free(buf);
-}
-
-static void
-update_features(Gputop__Features *features)
-{
-    gputop_string_t *str;
-    int i;
-    bool n_queries = 0;
-
-    devinfo.devid = features->devinfo->devid;
-    devinfo.n_eus = features->devinfo->n_eus;
-    devinfo.n_eu_slices = features->devinfo->n_eu_slices;
-    devinfo.eu_threads_count = features->devinfo->eu_threads_count;
-    devinfo.n_eu_sub_slices = features->devinfo->n_eu_sub_slices;
-    devinfo.subslice_mask = features->devinfo->subslice_mask;
-    devinfo.slice_mask = features->devinfo->slice_mask;
-
-    str = gputop_string_new("{ \"method\": \"features_notify\", \"params\": [ { \"oa_queries\": [\n");
-
-    if (IS_HASWELL(devinfo.devid)) {
-	_gputop_web_console_log("Adding Haswell queries\n");
-	gputop_oa_add_queries_hsw(&devinfo);
-    } else if (IS_BROADWELL(devinfo.devid)) {
-	_gputop_web_console_log("Adding Broadwell queries\n");
-	gputop_oa_add_queries_bdw(&devinfo);
-    } else if (IS_CHERRYVIEW(devinfo.devid)) {
-	_gputop_web_console_log("Adding Cherryview queries\n");
-	gputop_oa_add_queries_chv(&devinfo);
-    } else if (IS_SKYLAKE(devinfo.devid)) {
-	_gputop_web_console_log("Adding Skylake queries\n");
-	gputop_oa_add_queries_skl(&devinfo);
+    if (IS_HASWELL(devid)) {
+        _gputop_web_console_log("Adding Haswell queries\n");
+        gputop_oa_add_queries_hsw(&devinfo);
+    } else if (IS_BROADWELL(devid)) {
+        _gputop_web_console_log("Adding Broadwell queries\n");
+        gputop_oa_add_queries_bdw(&devinfo);
+    } else if (IS_CHERRYVIEW(devid)) {
+        _gputop_web_console_log("Adding Cherryview queries\n");
+        gputop_oa_add_queries_chv(&devinfo);
+    } else if (IS_SKYLAKE(devid)) {
+        _gputop_web_console_log("Adding Skylake queries\n");
+        gputop_oa_add_queries_skl(&devinfo);
     } else
-	assert_not_reached();
-
-    for (i = 0; i < I915_OA_METRICS_SET_MAX; i++) {
-	struct gputop_perf_query *query = &i915_perf_oa_queries[i];
-
-	if (query->name) {
-	    if (n_queries)
-		gputop_string_append(str, ",\n");
-	    append_i915_oa_query(str, query);
-	    n_queries++;
-	}
-    }
-
-    gputop_string_append(str, "],\n");
-    gputop_string_append_printf(str, " \"n_cpus\": %u,\n", features->n_cpus);
-    gputop_string_append(str, " \"cpu_model\": \"");
-    gputop_string_append_escaped(str, features->cpu_model);
-    gputop_string_append(str, "\",\n");
-    gputop_string_append(str, " \"kernel_release\": \"");
-    gputop_string_append_escaped(str, features->kernel_release);
-    gputop_string_append(str, "\",\n");
-    gputop_string_append(str, " \"kernel_build\": \"");
-    gputop_string_append_escaped(str, features->kernel_build);
-    gputop_string_append(str, "\"\n");
-    gputop_string_append(str, " } ] }\n");
-
-    _gputop_web_worker_post(str->str);
-    gputop_string_free(str, true);
-}
-
-static void
-forward_log(Gputop__Log *log)
-{
-    gputop_string_t *str = gputop_string_new("{ \"method\": \"log\", \"params\": [ [\n");
-    int i;
-
-    for (i = 0; i < log->n_entries; i++) {
-	Gputop__LogEntry *entry = log->entries[i];
-	char *msg = entry->log_message;
-	int j;
-
-	/* XXX: hack to remove newlines which mess up the json format... */
-	for (j = 0; msg[j]; j++) {
-	    if (msg[j] == '\n')
-		msg[j] = ' ';
-	}
-
-	gputop_string_append_printf(str, "    { \"level\": %d, \"message\": \"%s\" }",
-				    entry->log_level,
-				    msg);
-
-	if (i < (log->n_entries - 1))
-	    gputop_string_append(str, ",\n");
-	else
-	    gputop_string_append(str, "\n");
-
-    }
-
-    gputop_string_append_printf(str, "  ]\n], \"id\": %u }", next_rpc_id++);
-
-    _gputop_web_worker_post(str->str);
-    gputop_string_free(str, true);
-}
-
-static void
-forward_close_notify(Gputop__CloseNotify *notify)
-{
-    gputop_string_t *str = gputop_string_new("{ \"method\": \"close_notify\", ");
-
-    gputop_string_append_printf(str, "\"params\": [ %u ] }", notify->id);
-
-    _gputop_web_worker_post(str->str);
-    gputop_string_free(str, true);
-}
-
-static void
-forward_fill_notify(Gputop__BufferFillNotify *notify)
-{
-    gputop_string_t *str = gputop_string_new("{ \"method\": \"fill_notify\", ");
-
-    gputop_string_append_printf(str, "\"params\": [ %u, %u ] }", notify->query_id, notify->fill_percentage);
-
-    _gputop_web_worker_post(str->str);
-    gputop_string_free(str, true);
-}
-
-static void
-post_empty_reply(const char *req_uuid)
-{
-    char *str = NULL;
-
-    asprintf(&str, "{ \"params\": [ {} ], \"uuid\": \"%s\" }", req_uuid);
-
-    _gputop_web_worker_post(str);
-
-    free(str);
-}
-
-static void
-forward_error(Gputop__Message *message)
-{
-    gputop_string_t *str = gputop_string_new("{ \"params\": [ { \"error\": \"");
-    gputop_string_append_escaped(str, message->error);
-    gputop_string_append(str, "\"} ], \"uuid\": \"");
-    gputop_string_append(str, message->reply_uuid);
-    gputop_string_append(str, "\" }");
-
-    _gputop_web_worker_post(str->str);
-    gputop_string_free(str, true);
-}
-
-static void
-forward_ack(Gputop__Message *message)
-{
-    gputop_web_console_log("ACK: %s", message->reply_uuid);
-    post_empty_reply(message->reply_uuid);
-}
-
-static void
-handle_protobuf_message(uint8_t *data, int len)
-{
-    Gputop__Message *message;
-
-    _gputop_web_console_log("PROTOBUF MESSAGE RECEIVED\n");
-
-    message = (void *)protobuf_c_message_unpack(&gputop__message__descriptor,
-						NULL, /* default allocator */
-						len,
-						data);
-
-    switch (message->cmd_case) {
-    case GPUTOP__MESSAGE__CMD_ERROR:
-	/* FIXME: check if the message originated here */
-	forward_error(message);
-	break;
-    case GPUTOP__MESSAGE__CMD_ACK:
-	/* FIXME: check if the message originated here */
-	forward_ack(message);
-	break;
-    case GPUTOP__MESSAGE__CMD_FEATURES:
-	_gputop_web_console_log("Features message\n");
-	update_features(message->features);
-	break;
-    case GPUTOP__MESSAGE__CMD_LOG:
-	_gputop_web_console_log("Logs message\n");
-	forward_log(message->log);
-	break;
-    case GPUTOP__MESSAGE__CMD_CLOSE_NOTIFY:
-	forward_close_notify(message->close_notify);
-	break;
-    case GPUTOP__MESSAGE__CMD_FILL_NOTIFY:
-	forward_fill_notify(message->fill_notify);
-	break;
-    case GPUTOP__MESSAGE__CMD__NOT_SET:
-	assert_not_reached();
-    }
-
-    free(message);
+        assert_not_reached();
 }
 
 void EMSCRIPTEN_KEEPALIVE
 gputop_webworker_on_test(const char *msg, float val, const char *req_uuid)
 {
     gputop_web_console_log("test message from ui: (%s, %f)\n", msg, val);
-
-    post_empty_reply(req_uuid);
 }
 
+/*
 void EMSCRIPTEN_KEEPALIVE
 gputop_webworker_on_open_oa_query(uint32_t id,
 				  int perf_metric_set,
@@ -783,193 +589,11 @@ gputop_webworker_on_open_oa_query(uint32_t id,
     query->oa_query = &i915_perf_oa_queries[perf_metric_set];
     gputop_list_insert(open_queries.prev, &query->link);
 }
+*/
 
-void EMSCRIPTEN_KEEPALIVE
-gputop_webworker_on_close_oa_query(uint32_t id, char *req_uuid)
-{
-    Gputop__Request req = GPUTOP__REQUEST__INIT;
-    struct gputop_worker_query *query;
-
-    gputop_web_console_log("on_close_oa_query(%d)\n", id);
-
-    gputop_list_for_each(query, &open_queries, link) {
-	if (query->id == id) {
-	    req.uuid = req_uuid;
-	    req.req_case = GPUTOP__REQUEST__REQ_CLOSE_QUERY;
-	    req.close_query = id;
-
-	    send_pb_message(socket, &req.base);
-
-	    gputop_list_remove(&query->link);
-	    free(query);
-	    return;
-	}
-    }
-
-    gputop_web_console_warn("webworker: requested to close unknown query ID: %d\n", id);
-}
-
-void EMSCRIPTEN_KEEPALIVE
-gputop_webworker_on_open_tracepoint(uint32_t id,
-				    int pid,
-				    int cpu,
-				    const char *system,
-				    const char *event,
-				    unsigned overwrite,
-				    unsigned live_updates,
-				    const char *req_uuid)
-{
-    Gputop__Request req = GPUTOP__REQUEST__INIT;
-    Gputop__OpenQuery open = GPUTOP__OPEN_QUERY__INIT;
-    Gputop__TraceInfo trace = GPUTOP__TRACE_INFO__INIT;
-    struct gputop_worker_query *query = malloc(sizeof(*query));
-
-    gputop_web_console_log("on_open_trace pid=%d cpu=%d %s:%s overwrite=%s live=%s\n",
-			   pid, cpu, system, event, overwrite ? "true" : "false", live_updates ? "true" : "false");
-
-    open.id = id;
-    open.type_case = GPUTOP__OPEN_QUERY__TYPE_TRACE;
-    open.trace = &trace;
-    open.live_updates = live_updates;
-    open.overwrite = overwrite;
-
-    trace.pid = pid;
-    trace.cpu = cpu;
-    trace.system = (char*)system;
-    trace.event = (char*)event;
-
-    req.uuid = (char*)req_uuid;
-    req.req_case = GPUTOP__REQUEST__REQ_OPEN_QUERY;
-    req.open_query = &open;
-
-    send_pb_message(socket, &req.base);
-
-    memset(query, 0, sizeof(*query));
-    query->id = id;
-    gputop_list_insert(open_queries.prev, &query->link);
-}
-
-void EMSCRIPTEN_KEEPALIVE
-gputop_webworker_on_open_generic(uint32_t id,
-				 int pid,
-				 int cpu,
-				 unsigned type,	    /* XXX: emscripten apparently
-						     * gets upset with uint64_t args!?
-						     * might just need to test with
-						     * a more recent version.*/
-				 unsigned config,
-				 unsigned overwrite,
-				 unsigned live_updates,
-				 const char *req_uuid)
-{
-    Gputop__Request req = GPUTOP__REQUEST__INIT;
-    Gputop__OpenQuery open = GPUTOP__OPEN_QUERY__INIT;
-    Gputop__GenericEventInfo generic = GPUTOP__GENERIC_EVENT_INFO__INIT;
-    struct gputop_worker_query *query = malloc(sizeof(*query));
-
-    gputop_web_console_log("on_open_generic pid=%d, cpu=%d, type=%u config=%u overwrite=%d live=%s\n",
-			   pid, cpu, type, config, overwrite, live_updates ? "true" : "false");
-
-    open.id = id;
-    open.type_case = GPUTOP__OPEN_QUERY__TYPE_GENERIC;
-    open.generic = &generic;
-    open.live_updates = live_updates;
-    open.overwrite = overwrite;
-
-    generic.pid = pid;
-    generic.cpu = cpu;
-    generic.type = type;
-    generic.config = config;
-
-    req.uuid = (char*)req_uuid;
-    req.req_case = GPUTOP__REQUEST__REQ_OPEN_QUERY;
-    req.open_query = &open;
-
-    send_pb_message(socket, &req.base);
-
-    memset(query, 0, sizeof(*query));
-    query->id = id;
-    gputop_list_insert(open_queries.prev, &query->link);
-}
-
-/* Messages from the device... */
-static void
-gputop_websocket_onmessage(int socket, uint8_t *data, int len, void *user_data)
-{
-    //gputop_web_console_log("onmessage len=%d\n", len);
-
-    /* FIXME: don't use hardcoded enum values here! */
-    switch (data[0]) {
-    case 1: { /* WS_MESSAGE_PERF */
-	uint32_t id = *((uint32_t *)(data + 4));
-
-	//gputop_web_console_log("perf message, len=%d, id=%d\n", len, id);
-	handle_perf_message(id, data + 8, len - 8);
-        break;
-    }
-    case 2: /* WS_MESSAGE_PROTOBUF */
-	handle_protobuf_message(data + 8, len - 8);
-        break;
-    case 3: {/* WS_MESSAGE_I915_PERF */
-	uint32_t id = *((uint32_t *)(data + 4));
-
-	//gputop_web_console_log("perf message, len=%d, id=%d\n", len, id);
-	handle_i915_perf_message(id, data + 8, len - 8);
-        break;
-    }
-    }
-}
-
-static void
-gputop_websocket_onerror(int socket, void *user_data)
-{
-    gputop_web_console_log("websocket error\n");
-}
-
-static void
-gputop_websocket_onclose(int socket, void *user_data)
-{
-    gputop_web_console_log("websocket closed\n");
-}
-
-static void
-gputop_websocket_onopen(int socket, void *user_data)
-{
-    Gputop__Request req = GPUTOP__REQUEST__INIT;
-    char uuid_str[64];
-    uuid_t uuid;
-
-    uuid_generate(uuid);
-    uuid_unparse(uuid, uuid_str);
-
-    req.uuid = uuid_str;
-    req.req_case = GPUTOP__REQUEST__REQ_GET_FEATURES;
-    req.get_features = true;
-
-    send_pb_message(socket, &req.base);
-}
 
 void EMSCRIPTEN_KEEPALIVE
 gputop_webworker_init(void)
 {
-    const char *host = _gputop_web_get_location_host();
-    char *url = NULL;
-
-    asprintf(&url, "ws://%s/gputop/", host);
-
     gputop_list_init(&open_queries);
-
-    socket = _gputop_web_socket_new(url, "binary");
-    _gputop_web_socket_set_onopen(socket,
-				  gputop_websocket_onopen,
-				  NULL); /* user data */
-    _gputop_web_socket_set_onerror(socket,
-				   gputop_websocket_onerror,
-				   NULL); /* user data */
-    _gputop_web_socket_set_onclose(socket,
-				   gputop_websocket_onclose,
-				   NULL); /* user data */
-    _gputop_web_socket_set_onmessage(socket,
-				     gputop_websocket_onmessage,
-				     NULL); /* user data */
 }
